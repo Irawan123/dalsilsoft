7445ac41f82916ae3d7af87f47b5e1572c194425
Ahak <irawan.ahak@gmail.com>
2017-08-24 03:16:22 +0700

    all pyc files removed

--- dalsil_pkg_basic/model/__init__.py
+++ dalsil_pkg_basic/model/__init__.py
@@ -2,3 +2,4 @@ import config
 import master
 import check_giro
 import rent_truck
+import modified
--- dalsil_pkg_basic/model/modified/res_partner.py
+++ dalsil_pkg_basic/model/modified/res_partner.py
@@ -1,6 +1,7 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError, UserError
 
+
 class ResPartner(models.Model):
     """
     Res Partner
