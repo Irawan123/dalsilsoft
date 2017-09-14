6a3e911535e9551efe24dd7f927b6b58b40fc74d
Ahak <irawan.ahak@gmail.com>
2017-09-14 22:58:35 +0700

    Ganti urutan menu & bug fixes

--- l10n_dalsil/__manifest__.py
+++ l10n_dalsil/__manifest__.py
@@ -5,11 +5,11 @@
     "author": "Dalsil Soft",
     "depends": ["ss_common", "account", "stock_account"],
     "data": [
-        # "data/l10n_dalsil_chart_data.xml",
-        # "data/account.account.template.csv",
-        # "data/account.chart.template.csv",
-        # "data/account.tax.template.csv",
-        # "data/account_chart_template_data.yml",
-        # "data/product_category_default.xml"
+        "data/l10n_dalsil_chart_data.xml",
+        "data/account.account.template.csv",
+        "data/account.chart.template.csv",
+        "data/account.tax.template.csv",
+        "data/account_chart_template_data.yml",
+        "data/product_category_default.xml"
     ],
 }
