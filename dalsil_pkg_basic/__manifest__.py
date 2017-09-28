c5868d576269d92e3ea480c2898984bf3fb75257
Ahak <irawan.ahak@gmail.com>
2017-09-28 22:19:42 +0700

    tambah sequence ke manifest

--- dalsil_pkg_basic/__manifest__.py
+++ dalsil_pkg_basic/__manifest__.py
@@ -9,11 +9,15 @@
         "base", "product", "stock", "sale", "account", "ss_common", "account_accountant", "account_financial_report_qweb"
     ],
     "data": [
+        'data/sequence.xml',
+
         'view/modified/res_partner.xml',
         'view/modified/account_invoice.xml',
         'view/modified/product_template.xml',
+
         'view/config/config.xml',
         'view/config/wiz_config.xml',
+
         'view/master/truck.xml',
         'view/fee_sales/fee_sales.xml',
         'view/rent_truck/rent_truck.xml',
@@ -21,6 +25,7 @@
         'view/rent_truck/wiz_rent_truck.xml',
         'view/rent_truck/wiz_rent_truck_line_invoice.xml',
         'view/rent_truck/wiz_rent_truck_line_purchase.xml',
+
         'view/check_giro/in_check_giro.xml',
         'view/check_giro/in_check_giro_line.xml',
         'view/check_giro/out_check_giro.xml',
